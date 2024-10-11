b43bd0912706bd6c0ad432e6ccfe10b75726c95d
jasaga2401 <jasaga@protonmail.com>
2024-10-11 23:18:26 +0100

    Add project files.

---
+++ calculateArea/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.lblWidth = New System.Windows.Forms.Label()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWidth.Location = New System.Drawing.Point(111, 163)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(250, 34)
+        Me.txtWidth.TabIndex = 0
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHeight.Location = New System.Drawing.Point(111, 243)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(250, 34)
+        Me.txtHeight.TabIndex = 1
+        '
+        'lblWidth
+        '
+        Me.lblWidth.AutoSize = True
+        Me.lblWidth.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWidth.Location = New System.Drawing.Point(111, 131)
+        Me.lblWidth.Name = "lblWidth"
+        Me.lblWidth.Size = New System.Drawing.Size(69, 25)
+        Me.lblWidth.TabIndex = 2
+        Me.lblWidth.Text = "Width:"
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeight.Location = New System.Drawing.Point(111, 212)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(74, 25)
+        Me.lblHeight.TabIndex = 3
+        Me.lblHeight.Text = "Height:"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.Location = New System.Drawing.Point(111, 308)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(250, 46)
+        Me.btnEnter.TabIndex = 4
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'txtArea
+        '
+        Me.txtArea.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtArea.Location = New System.Drawing.Point(455, 244)
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.Size = New System.Drawing.Size(250, 34)
+        Me.txtArea.TabIndex = 5
+        '
+        'lblArea
+        '
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblArea.Location = New System.Drawing.Point(450, 212)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(60, 25)
+        Me.lblArea.TabIndex = 6
+        Me.lblArea.Text = "Area:"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(106, 63)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(390, 29)
+        Me.lblTitle.TabIndex = 7
+        Me.lblTitle.Text = "Calculate the area of a rectangle"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.lblArea)
+        Me.Controls.Add(Me.txtArea)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.lblWidth)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents lblWidth As Label
+    Friend WithEvents lblHeight As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents txtArea As TextBox
+    Friend WithEvents lblArea As Label
+    Friend WithEvents lblTitle As Label
+End Class
---
+++ calculateArea/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Org.BouncyCastle.Crypto.Parameters
+
+Public Class Form1
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Dim width As String = Convert.ToInt32(txtWidth.Text)
+        Dim length As String = Convert.ToInt32(txtHeight.Text)
+
+        ' database connection string
+        Dim connectionString As String = "Server=localhost; Database=dbuser; User ID=root; Password=12Yellow34!"
+
+        Using conn As New MySqlConnection(connectionString)
+
+            conn.Open()
+
+            Using cmd As New MySqlCommand("sp_calculate_area", conn)
+                cmd.CommandType = CommandType.StoredProcedure
+
+                ' using stored procedure to calculate area
+                cmd.Parameters.AddWithValue("@width", width)
+                cmd.Parameters.AddWithValue("@length", length)
+
+                ' output parameter from stored procedure
+                Dim areaParam As New MySqlParameter("@area", MySqlDbType.Int32)
+                areaParam.Direction = ParameterDirection.Output
+                cmd.Parameters.Add(areaParam)
+
+                ' execute the stored procedure
+                cmd.ExecuteNonQuery()
+
+                ' get the output parameter value and output to the text box
+                Dim area As Integer = Convert.ToInt32(cmd.Parameters("@area").Value)
+                txtArea.Text = area.ToString()
+
+            End Using
+        End Using
+
+    End Sub
+End Class
